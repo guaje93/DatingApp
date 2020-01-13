f18e978e9523898518216e6ccdca458a263f92ec
Jakub <jakub.szymczak93@gmail.com>
2020-01-13 19:17:15 +0100

    Initial API

---
+++ DatinApp.api/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatinApp.api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatinApp.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext dataContext)
+        {
+            this._context = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var values = await this._context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var value = await this._context.Values.FirstOrDefaultAsync(p => p.ID == id);
+            return Ok(value);
+        }
+
+         [HttpPost]
+         public void Post([FromBody] string value){
+
+         }
+
+
+    }
+}
---
+++ DatinApp.api/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatinApp.api.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatinApp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+       
+       public DbSet<Value> Values { get; set; }
+        
+    }
+}
